cc27fe93b9bada41a44b233b04e3060cac952751
Madsmarkmus <75371111+Madsmarkmus@users.noreply.github.com>
2020-12-03 21:54:05 +0100

    Update fra DESKTOP
    Fra Juniorrobot til robot

--- Madsmarkmus.java
+++ Madsmarkmus.java
@@ -4,59 +4,57 @@ import robocode.*;
 public class Madsmarkmus extends Robot
 {
 	public void run() {
-	
-		setAdjustRadarForGunTurn(true);
 		
 		//HVIS START I MARGIN
-		if (getX < 50){
-			turnTo(90);
-			ahead(50-getX);
-			turnTo(0);
+		if (getX() < 50){
+			turnRight(90);
+			ahead(50-getX());
+			turnLeft(0);
 		}
-		if (getX > getBattleFieldWidth-50){
-			turnTo(270);
-			ahead(50-(getBattleFieldWidth-getX));
-			turnTo(180);
+		if (getX() > getBattleFieldWidth()-50){
+			turnRight(270);
+			ahead(50-(getBattleFieldWidth()-getX()));
+			turnLeft(180);
 		} 
-		if (getY < 50){
-			turnTo(0);
-			ahead(50-getY);
-			turnTo(270);
+		if (getY() < 50){
+			turnRight(0);
+			ahead(50-getY());
+			turnLeft(270);
 		}
-		if (getY > getBattleFieldWidth-50){
-			turnTo(180);
-			ahead(50-(fieldHeight-getY));
-			turnTo(90);
+		if (getY() > getBattleFieldWidth()-50){
+			turnRight(180);
+			ahead(50-(getBattleFieldHeight()-getY()));
+			turnLeft(90);
 		}
 		
 		//HVIS PÅ KØRELINJEN
 		
-		if (getX == 50){
-			turnTo(0);
-			ahead(fieldHeight-getY-50);
+		if (getX() == 50){
+			turnRight(0);
+			ahead(getBattleFieldHeight()-getY()-50);
 		}
-		if (getX == (getBattleFieldWidth-50)){
-			turnTo(180);
-			ahead(getY-50);
+		if (getX() == (getBattleFieldWidth()-50)){
+			turnRight(180);
+			ahead(getY()-50);
 		}
-		if (getY == 50) {
-			turnTo(270);
-			ahead(getBattleFieldWidth-getY-50);
+		if (getY() == 50){
+			turnRight(270);
+			ahead(getBattleFieldWidth()-50-50);
 		}
-		if (getY == (fieldHeight-50)) {
-			turnTo(90);
-			ahead(getBattleFieldWidth-getX-50);
+		if (getY() == (getBattleFieldHeight()-50)) {
+			turnRight(90);
+			ahead(getBattleFieldWidth()-getX()-50);
 		}
 
 		//HVIS START I MIDTEN
 		
-		if (getX <= (getBattleFieldWidth*0.5) && getY < fieldHeight-50 && getY > 50) {
-		turnTo(270);
-		ahead(getBattleFieldWidth-(getBattleFieldWidthgetX)-50);
+		if (getX() <= (getBattleFieldWidth()*0.5) && getY() < getBattleFieldHeight()-50 && getY() > 50) {
+		turnLeft(270);
+		ahead(getBattleFieldWidth()-(getBattleFieldWidth()-getX())-50);
 		}
-		if (getX > (getBattleFieldWidth*0.5) && getY < fieldHeight-50 && getY > 50) {
-		turnTo(90);
-		ahead(getBattleFieldWidth-getX-50);
+		if (getX() > (getBattleFieldWidth()*0.5) && getY() < getBattleFieldHeight()-50 && getY() > 50) {
+		turnRight(90);
+		ahead(getBattleFieldWidth()-getX()-50);
 		}	
 	}
 }
